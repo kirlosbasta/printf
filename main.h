d3b90a50b563e0df78eb1e991caeed1f70f27690
kirlos basta <kirlosbasta@gmail.com>
2023-10-15 05:38:26 +0300

    Add first files for %c specifier

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int _strlen(const char *str);
+void *_memcpy(char *dest, char *src, unsigned int n);
+char *char_handler(char *buffer, char character, unsigned int * buffer_size, unsigned int *index);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
