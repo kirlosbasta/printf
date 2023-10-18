1515b29558f87aaefed187b9deff809686f9e14f
kirlos basta <kirlosbasta@gmail.com>
2023-10-19 01:35:18 +0300

    Fix the error

--- format.c
+++ format.c
@@ -41,11 +41,12 @@ int format_handler(va_list ap, f_list list[], const char *format)
 	}
 	buffer[bi] = '\0';
 	buffer_size = _strlen(buffer);
-	write(1, buffer, buffer_size);
-	free(buffer);
 	if (buffer_size == 0)
 	{
+		buffer[bi] = '\n';
 		buffer_size++;
 	}
+	write(1, buffer, buffer_size);
+	free(buffer);
 	return (buffer_size);
 }
